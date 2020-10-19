a9477be7e9a160a02121c3dbe6bfe8b3b193a24d
Trifle22 <xromchenkov99@gmail.com>
2020-10-20 00:00:45 +0300

    + section.specialties +slider +some animation

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var mySwiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  loop: true,
+  spaceBetween: 100,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+});
