f5d5eac4594cc92c56686b83005208a01bbb2194
Trifle22 <xromchenkov99@gmail.com>
2020-11-02 02:09:51 +0300

    full done

--- js/main.js
+++ js/main.js
@@ -7,3 +7,32 @@ var mySwiper = new Swiper('.swiper-container', {
     type: 'bullets',
   },
 });
+
+let tab = function () {
+  let tabs = 
+  document.querySelectorAll('.tab');
+  let tabContent = 
+  document.querySelectorAll('.tabs-content');
+  let tabName;
+
+  tabs.forEach(item => {
+    item.addEventListener('click', selectTab);
+  });
+
+  function selectTab() {
+  tabs.forEach(item => {
+    item.classList.remove('is-active');
+  });
+  this.classList.add('is-active');
+  tabName = this.getAttribute('data-tab-name');
+  selectTabContent(tabName);
+  }
+  function selectTabContent(tabName) {
+    tabContent.forEach(item => {
+      item.classList.contains(tabName) ?
+      item.classList.add('is-active') : item.classList.remove('is-active');
+    });
+  }
+};
+
+tab();
